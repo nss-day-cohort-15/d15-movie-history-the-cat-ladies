502ab5ba46de838b17d0ab4613211d75504a1c9f
Casey Reed <casey.w.reed@gmail.com>
2016-08-26 09:49:16 -0500

    Auth running and storing userID

--- src/js/events.js
+++ src/js/events.js
@@ -1,5 +1,24 @@
 "use strict";
 
-$( document ).ready(function() {
-    console.log( "ready!" );
-});
+let login = require('./user.js')
+
+let userID = null;
+
+function setEvents () {
+    $("#auth-button").click(function() {
+        console.log('auth running')
+        login()
+        .then(function (result) {
+        console.log("current user", result.user)
+        let user = result.user
+        userID = user.uid
+        console.log("user ID", userID)
+          })
+    });
+}
+
+function getUserID () {
+    return userID;
+}
+
+module.exports = {setEvents, getUserID}
