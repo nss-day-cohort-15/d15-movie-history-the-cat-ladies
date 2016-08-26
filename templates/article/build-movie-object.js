9afb6bc84c49acff0aa331fc400b465b638deea3
Casey Reed <casey.w.reed@gmail.com>
2016-08-26 10:39:49 -0500

    Added movie templates and object functions

---
+++ templates/article/build-movie-object.js
@@ -0,0 +1,16 @@
+"use strict";
+
+let simpleMovieTemplate = require('./simpleMovieTemplate.hbs');
+let complexMovieTemplate = require('./complexMovieTemplate.hbs');
+
+function buildSimpleMovieObj(movieData) {
+    let simpleMovie = {
+        title: null,
+        year,
+        posterURL:
+    }
+}
+
+function buildComplexMovieObj(movieData) {
+
+}
